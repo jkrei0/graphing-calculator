cacbf43adaffdb6fbd4304a9043cb938a6b4ff2e
jkrei0 <jkrei021@gmail.com>
2023-01-14 16:11:18 -0500

    Initial calculator v2

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Graphing Calculator
 A simple c++ graphing calculator.
 
+## Version 2
+Not complete yet.
+
+### Building/Running
+
+#### Building with g++
+1. make sure you have g++ installed, then run
+2. `g++ new.cpp -o new`
+3. Run `new.exe`
+
+#### Building in VSCode
+A simple .vscode folder is included for building/debugging.
+If you want to use it:
+1. Make sure you have g++ and gdb installed.
+2. Edit `launch.json` and `tasks.json` and change `command` and `miDebuggerPath` to point to your g++/gdb executables
+3. Open the `new.cpp` file in the editor
+4. Click `Run -> Start Debugging` or press `F5`
+
+---
+
+## Version 1
 When run, type 'h' or 'help' for a list of options.
 
 When entering equations, refer to the following modes and syntax structure
@@ -22,7 +43,7 @@ Operations are evaluated in the order they appear in this list, then in left-to-
 
 ---
 
-## Bugs
+### Bugs
 Bugs nearest the top of the list are ones that will likely get fixed first
 
 - When entering an equation *starting* with addition or subtraction, (eg `+2` or `-x`), a segmentation fault occurs, seemlingly on line `209-210` (the very beginning of `solve->GET`)
@@ -31,16 +52,16 @@ Bugs nearest the top of the list are ones that will likely get fixed first
 
 ---
 
-## Building/Running
+### Building/Running
 
-### Building with g++
+#### Building with g++
 1. make sure you have g++ installed, then run
 
 2. `g++ main.cpp -o main`
 
 3. Run `main.exe`
 
-### Building in VSCode
+#### Building in VSCode
 A simple .vscode folder is included for building/debugging.
 If you want to use it:
 1. Make sure you have g++ and gdb installed.
---
+++ new.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <regex>
+
+std::string getLine(std::string prompt) {
+    std::string input{ };
+    // Prompt for input
+    std::cout << prompt;
+    std::getline(std::cin >> std::ws, input); // ws to ignore leading whitespace.
+    return input;
+}
+
+enum class o {
+    none,
+    add,
+    subtract,
+    negate,
+    multiply,
+    divide,
+    exponent
+};
+enum class t {
+    none,
+    group,
+    number,
+    operation,
+    variable
+};
+struct Token {
+    t type{ t::none };
+    std::vector<Token> group{ };
+    double number{ 0 };
+    o operation{ o::none };
+    char variable{ '&' };
+};
+using TokenArr = std::vector<Token>;
+
+std::string tAsString(t name) {
+    switch(name) {
+        case t::none: return "none";
+        case t::group: return "group";
+        case t::number: return "number";
+        case t::operation: return "operation";
+        case t::variable: return "variable";
+        default: return "t?";
+    }
+}
+std::string oAsString(o name) {
+    switch(name) {
+        case o::none: return "none";
+        case o::add: return "add";
+        case o::subtract: return "subtract";
+        case o::negate: return "negate";
+        case o::multiply: return "multiply";
+        case o::divide: return "divide";
+        case o::exponent: return "exponent";
+        default: return "o?";
+    }
+}
+void printToken(Token tk) {
+    std::cout << tAsString(tk.type) << "= " << oAsString(tk.operation) << ", " << tk.number << ", " << tk.variable << "\n";
+    if (tk.type == t::group) {
+        for (const Token& child : tk.group) {
+           std::cout << "\t";
+           printToken(child);
+        }
+        std::cout << "<-\n";
+    }
+}
+
+TokenArr tokenize(std::string equation) {
+    TokenArr out{ };
+    const std::regex rGroup{ "^\\(" };
+    const std::regex rNumber{ "^\\d+(\\.\\d+)?|\\.\\d+" };
+    const std::regex rOperator{ "^(\\+|-|\\*|\\/|\\^)" };
+    const std::regex rVariable{ "^[a-z]" };
+    const std::regex rWhitespace{ "^ +" };
+    std::smatch match;
+
+    while (true) {
+        if (std::regex_search(equation, match, rNumber)) {
+            Token newTk{ t::number };
+            newTk.number = std::stod(match.str());
+            out.push_back(newTk);
+
+        } else if (std::regex_search(equation, match, rOperator)) {
+            Token newTk{ t::operation };
+            switch (match.str()[0]) {
+                case '+': newTk.operation = o::add;      break;
+                case '-': newTk.operation = o::subtract; break;
+                case '*': newTk.operation = o::multiply; break;
+                case '/': newTk.operation = o::divide;   break;
+                case '^': newTk.operation = o::exponent; break;
+                default:
+                    std::cout << "<!> [tokenize:0] Unknown operator " << match.str() << '\n';
+            }
+            out.push_back(newTk);
+
+        } else if (std::regex_search(equation, match, rVariable)) {
+            Token newTk{ t::variable };
+            newTk.variable = match.str().at(0);
+            out.push_back(newTk);
+            
+        } else if (std::regex_search(equation, match, rGroup)) {
+            Token newTk{ t::group };
+
+            std::size_t pos{ 1 };
+            std::size_t depth{ 0 };
+            while (pos < equation.size()) {
+                if (equation.at(pos) == ')') {
+                    if (depth == 0) break;
+                    else depth -= 1;
+                
+                } else if (equation.at(pos) == '(') {
+                    depth += 1;
+                }
+                pos++;
+            }
+            if (depth > 0) {
+                std::cout << "<!> [tokenize:1] Unmatched parentheses: " << equation << '\n';
+                break;
+            }
+            newTk.group = tokenize(equation.substr(1, pos-1));
+            out.push_back(newTk);
+            equation = equation.substr(pos, equation.size());
+            
+        } else if (!std::regex_search(equation, match, rWhitespace)) {
+            // No match and not whitespace
+            if (equation.size() > 0) {
+                // It's only a problem if we're not done already.
+                std::cout << "<!> [tokenize:2] Cannot parse part of equation: " << equation << '\n';
+            }
+            break;
+        }
+
+        equation = equation.substr(match.length(), equation.size());
+    }
+
+    return out;
+}
+/** Prepares a TokenArr for parsing. OPERATES DIRECTLY ON THE PASSED LIST
+ * - replaces o::subtract with o::negate or o::add o::negate
+ */
+bool clean(TokenArr& list) {
+    if (list.at(0).operation == o::subtract) {
+        list.at(0).operation = o::negate;
+    }
+    if (list.at(list.size()-1).type == t::operation) {
+        std::cout << "<!> [clean:0] Equation contains a trailing operator\n";
+        return false;
+    }
+    // First & last already checked, so loop from [1, -1]
+    for (std::size_t i{ 1 }; i < list.size()-1; i++) {
+        if (list.at(i).type == t::operation &&
+            list.at(i-1).type == t::operation &&
+            list.at(i+1).type == t::operation)
+        {
+            std::cout << "<!> [clean:1] Equation contains too many successive operators (3+) at position " << i << '\n';
+            for (const Token& tk : list) {
+                printToken(tk);
+            }
+            return false;
+        }
+
+        if (list.at(i).operation == o::subtract && list.at(i+1).operation == o::subtract) {
+            // Replace double negatives with addition
+            list.at(i).operation = o::add;
+            list.erase(list.begin()+i+1);
+
+        } else if (list.at(i).operation == o::subtract && list.at(i-1).type == t::operation) {
+            // Eg 3+(subtract)4 => 3+(negate)4
+            list.at(i).operation = o::negate;
+
+        } else if (list.at(i).operation == o::subtract) {
+            // Previous now must be a value
+            // Eg 3(subtract)4 => 3+(negate)4
+            // Edge case 3-+4 => 3+-+ 4 => Error on next check & I don't
+            // have to worry about it here!
+            list.at(i).operation = o::add;
+            Token newTk{ t::operation };
+            newTk.operation = o::negate;
+            list.insert(list.begin()+i, newTk);
+        }
+    }
+    return true;
+}
+
+int main() {
+    std::cout << "GRAPHING CALCULATOR v2\n";
+    std::string equation{ getLine("> ") };
+    TokenArr tokenized{ tokenize(equation) };
+    // if (!clean(tokenized)) {
+    //     std::cout << "<!> [main:0] Cleaning failed\n";
+    // }
+    for (const Token& tk : tokenized) {
+        printToken(tk);
+    }
+
+    // std::regex regex{ "\\(" };
+    // std::smatch match;
+    // std::regex_search(equation, match, regex);
+    // std::cout << "Match: " << match.str() << "\n";
+
+    std::cout << "\nDone!\n";
+    return 0;
+}
